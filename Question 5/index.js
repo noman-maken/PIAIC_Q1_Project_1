ab979d12de0c27530c432238f404e3d7e7bede72
Muhammad Noman Aslam Maken <mnomanaslam6@gmail.com>
2023-02-18 23:08:51 +0500

    Question 5

---
+++ Question 5/index.js
@@ -0,0 +1,5 @@
+"use strict";
+//Name: Muhammad Noman Aslam (PIAIC77162)
+const famous_person = "Albert Einstein";
+const message = 'once said, “A person who never made a mistake never tried anything new.”';
+console.log(`${famous_person} ${message}`);
---
+++ Question 5/index.ts
@@ -0,0 +1,7 @@
+//Name: Muhammad Noman Aslam (PIAIC77162)
+
+const famous_person: string = "Albert Einstein";
+const message: string = 'once said, “A person who never made a mistake never tried anything new.”';
+
+console.log(`${famous_person} ${message}`);
+
